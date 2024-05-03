226ea15c4adbfffcc5e81879131b53a4dee0f4e9
hoel <public.hoelzlwimmer@tum.de>
2024-05-03 13:04:15 +0200

    add Download parameter in docker-compose.dev.yml

--- build.sh
+++ build.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
 echo "Starting setup of licensed DB"
-./setup_data.sh /data/nedrex_files; ./build.py update --conf .licensed_config.toml --download
+if [ "${DOWNLOAD_ON_STARTUP}" == "1" ]; then
+     echo "Download: ON"
+     ./setup_data.sh /data/nedrex_files; ./build.py update --conf .licensed_config.toml --download
+  else
+     echo "Download: OFF"
+     ./setup_data.sh /data/nedrex_files; ./build.py update --conf .licensed_config.toml
+  fi
+
 #./build.py update --conf .licensed_config.toml
 ./set_metadata.py --config .licensed_config.toml --version live
 ./clean_volumes.sh licensed
