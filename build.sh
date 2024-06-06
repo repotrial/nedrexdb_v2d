eb48d24b9d10581eb24a8818815a24e72a7569cc
Andreas Maier <andreas.maier-1@uni-hamburg.de>
2024-06-06 13:43:09 +0200

    Small fixes and improvements (#6)
    * made dev mongo express port variable to circumvent port in use issues
    
    * added build to launch script
    
    * Add Download Flag & is_reviewed (#5)
    
    * add changes from old repository: add comments, add chembl downloader, add is_reviewed param to protein node
    
    * add Download parameter in docker-compose.dev.yml
    
    * clean up for pull request - revert path & download Setting
    
    * clean up for pull request - revert path & download Setting
    
    ---------
    
    Co-authored-by: hoel <public.hoelzlwimmer@tum.de>
    
    * Update docker-compose.dev.yml
    
    fixed typo in env variable
    
    * Update docker-compose.yml
    
    added env var to prod docker-compose.yaml
    
    * fixed repotrial importer
    
    * removed static file download from Download OFF build script
    
    ---------
    
    Co-authored-by: ahoewi <112104849+ahoewi@users.noreply.github.com>
    Co-authored-by: hoel <public.hoelzlwimmer@tum.de>

--- build.sh
+++ build.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
 echo "Starting setup of licensed DB"
-./setup_data.sh /data/nedrex_files; ./build.py update --conf .licensed_config.toml --download
-#./build.py update --conf .licensed_config.toml
+if [ "${DOWNLOAD_ON_STARTUP}" == "1" ]; then
+     echo "Download: ON"
+     ./setup_data.sh /data/nedrex_files; ./build.py update --conf .licensed_config.toml --download
+  else
+     echo "Download: OFF"
+     ./build.py update --conf .licensed_config.toml
+  fi
+
+./build.py update --conf .licensed_config.toml
 ./set_metadata.py --config .licensed_config.toml --version live
 ./clean_volumes.sh licensed
 echo "Finished setup of licensed DB"
