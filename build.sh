1f5abdf7e303a44afcda8e4431bf210f57314b4e
Andreas Maier <andreas.maier-1@uni-hamburg.de>
2024-06-17 23:54:00 +0200

    adding additional development options; fixed numpy version

--- build.sh
+++ build.sh
@@ -9,12 +9,15 @@ if [ "${DOWNLOAD_ON_STARTUP}" == "1" ]; then
      if [ "${SKIP_UPDATE}" == "1" ]; then
        echo "only restarting"
        ./build.py restart-live --conf .licensed_config.toml
-       ./clean_volumes.sh licensed
      else
        ./build.py update --conf .licensed_config.toml
        ./set_metadata.py --config .licensed_config.toml --version live
-       ./clean_volumes.sh licensed
      fi
+     if [ "${SKIP_CLEAN}" == "1" ]; then
+        echo "Skipping clean"
+      else
+        ./clean_volumes.sh licensed
+      fi
   fi
 echo "Finished setup of licensed DB"
 
@@ -25,10 +28,13 @@ fi
 echo "Starting setup of open DB"
 if [ "${SKIP_UPDATE}" == "1" ]; then
   ./build.py restart-live --conf .open_config.toml
-  ./clean_volumes.sh open
 else
   ./build.py update --conf .open_config.toml
   ./set_metadata.py --config .open_config.toml --version live
+fi
+if [ "${SKIP_CLEAN}" == "1" ]; then
+  echo "Skipping clean"
+else
   ./clean_volumes.sh open
 fi
 echo "Finished setup of open DB"
