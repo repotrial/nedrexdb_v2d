379df6c87737c9935ad8ab8dd8c5f5fb004edd70
JohannesKersting <kerstingjohannes@gmail.com>
2025-02-03 18:41:41 +0100

    fixed downloader for intogen

--- nedrexdb/downloaders/intogen.py
+++ nedrexdb/downloaders/intogen.py
@@ -33,6 +33,8 @@ def download_intogen():
     d = Downloader(
         url=url,
         target=intogen_dir / zip_fname,
+        username=None,
+        password=None,
     )
     d.download()
     with _cd(intogen_dir):
