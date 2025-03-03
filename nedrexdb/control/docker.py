0e33be0140bf56faec7b99371a4493adc34a3fab
Andreas Maier <andreas.maier-1@uni-hamburg.de>
2025-03-03 14:31:07 +0100

    fixing neo4j shutdown issue

--- nedrexdb/control/docker.py
+++ nedrexdb/control/docker.py
@@ -245,11 +245,11 @@ class _NeDRexBaseInstance(_NeDRexInstance):
             bool: True if shutdown was successful, False otherwise
         """
         if not self._stop_neo4j_process():
-            print("Failed to gracefully stop Neo4j process, forcing now")
-            self.neo4j_container.remove(force=True)
-
-        if not self._stop_neo4j_container():
-            print("Failed to stop container")
+            print("Failed to gracefully stop Neo4j process")
+            # try:
+            #     self.neo4j_container.remove()
+            # except Exception:
+            #     pass
 
         return self._remove_neo4_container()
 
@@ -348,10 +348,10 @@ class _NeDRexBaseInstance(_NeDRexInstance):
 
     def set_up(self, use_existing_volume=True, neo4j_mode="db"):
         print("Setting up Live NeDRex instance...")
+        self._set_up_neo4j(use_existing_volume=use_existing_volume, neo4j_mode=neo4j_mode)
         if neo4j_mode != "db-write":
             self._set_up_mongo(use_existing_volume=use_existing_volume)
             self._set_up_express()
-        self._set_up_neo4j(use_existing_volume=use_existing_volume, neo4j_mode=neo4j_mode)
 
     def remove(self, remove_db_volume=False, remove_configdb_volume=True, neo4j_mode="db"):
         self._remove_mongo(
