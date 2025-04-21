aac1a13c0cd3d27da3e24286b51f238551fef498
Andreas Maier <andreas.maier-1@uni-hamburg.de>
2025-04-21 13:37:57 +0200

    setting default for api_key correctly

--- nedrexdb/llm.py
+++ nedrexdb/llm.py
@@ -4,8 +4,4 @@ _LLM_BASE=_config["embeddings.server_base"]
 _LLM_model=_config[f"embeddings.model"]
 _LLM_path=_config[f"embeddings.path"]
 
-_LLM_API_KEY=_config[f"embeddings.api_key"]
-if _LLM_API_KEY is None:
-    _LLM_API_KEY="no-key"
-elif _LLM_API_KEY == "":
-    _LLM_API_KEY="no-key"
+_LLM_API_KEY=_config[f"embeddings"].get("api_key", "")
