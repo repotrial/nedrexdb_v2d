cdbb2fb2efea72c09b01bfd9c2e6798297693cb9
hoel <public.hoelzlwimmer@tum.de>
2025-11-05 17:25:16 +0100

    bugfixes for rare bugs in update

--- launch_docker.sh
+++ launch_docker.sh
@@ -1,5 +1,22 @@
 #!/usr/bin/bash
 
+# add c as a cmd option without argument.
+while getopts c name
+do
+        case $name in
+          c)no_cache=1;;
+          *)echo "Invalid arg $name";;
+        esac
+done
+
 docker network create nedrexdb_default
-docker compose -f docker-compose.dev.yml build
-docker compose -f docker-compose.dev.yml up -d
+
+# decide whether to use cache or not
+if [[ ! -z $no_cache ]]
+then
+  docker compose -f docker-compose.dev.yml build --no-cache
+  docker compose -f docker-compose.dev.yml up -d --force-recreate
+else
+  docker compose -f docker-compose.dev.yml build
+  docker compose -f docker-compose.dev.yml up -d
+fi
